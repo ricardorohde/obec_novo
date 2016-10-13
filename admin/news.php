94a6a8c8c23a294ce71e1fc943f1805316ced3f5
BibianaDavila <davila.bibiana@gmail.com>
2016-10-13 16:16:43 -0300

    Criada categoria para eventos na pg de notícias

--- admin/news.php
+++ admin/news.php
@@ -131,102 +131,207 @@
 								<!-- */\/* título *\/\* -->
 								<div class="contentAdm-title">GERENCIAR</div>
 
-								<table class="table table-hover admin-table">
-			    					
-			    					<thead>
-			      						<tr>
-			        						<th>Título</th>
-			        						<th class="hidden-xs">Data</th>
-			        						<th class="hidden-xs">Tipo</th>
-			        						<th>Editar</th>
-			        						<th>Excluir</th>
-			      						</tr>
-			    					</thead>
-			    					
-			    					<tbody>
-			      						<tr>
-			        						<td>UNESCO E CISAC LANÇAM MAPEAMENTO GLOBAL DAS INDÚSTRIAS CULTURAIS E CRIATIVAS</td>
-			        						<td class="hidden-xs">04/10/2016</td>
-									        <td class="hidden-xs"><div class="tableAdm-icon news" title="Notícia"></div></td>
-									        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
-									        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
-			      						</tr>
-			      						<tr>
-									        <td>EMPREENDEDORES BRASILEIROS PARTICIPAM DO MICSUL 2016</td>
-									        <td class="hidden-xs">03/10/2016</td>
-									        <td class="hidden-xs"><div class="tableAdm-icon event" title="Evento"></div></td>
-									        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
-									        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
-			      						</tr>
-			      						<tr>
-			        						<td>UNESCO E CISAC LANÇAM MAPEAMENTO GLOBAL DAS INDÚSTRIAS CULTURAIS E CRIATIVAS</td>
-			        						<td class="hidden-xs">04/10/2016</td>
-									        <td class="hidden-xs"><div class="tableAdm-icon news" title="Notícia"></div></td>
-									        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
-									        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
-			      						</tr>
-			      						<tr>
-									        <td>EMPREENDEDORES BRASILEIROS PARTICIPAM DO MICSUL 2016</td>
-									        <td class="hidden-xs">03/10/2016</td>
-									        <td class="hidden-xs"><div class="tableAdm-icon event" title="Evento"></div></td>
-									        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
-									        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
-			      						</tr>
-			      						<tr>
-			        						<td>UNESCO E CISAC LANÇAM MAPEAMENTO GLOBAL DAS INDÚSTRIAS CULTURAIS E CRIATIVAS</td>
-			        						<td class="hidden-xs">04/10/2016</td>
-									        <td class="hidden-xs"><div class="tableAdm-icon news" title="Notícia"></div></td>
-									        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
-									        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
-			      						</tr>
-			      						<tr>
-									        <td>EMPREENDEDORES BRASILEIROS PARTICIPAM DO MICSUL 2016</td>
-									        <td class="hidden-xs">03/10/2016</td>
-									        <td class="hidden-xs"><div class="tableAdm-icon event" title="Evento"></div></td>
-									        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
-									        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
-			      						</tr>
-			      						<tr>
-			        						<td>UNESCO E CISAC LANÇAM MAPEAMENTO GLOBAL DAS INDÚSTRIAS CULTURAIS E CRIATIVAS</td>
-			        						<td class="hidden-xs">04/10/2016</td>
-									        <td class="hidden-xs"><div class="tableAdm-icon news" title="Notícia"></div></td>
-									        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
-									        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
-			      						</tr>
-			      						<tr>
-									        <td>EMPREENDEDORES BRASILEIROS PARTICIPAM DO MICSUL 2016</td>
-									        <td class="hidden-xs">03/10/2016</td>
-									        <td class="hidden-xs"><div class="tableAdm-icon event" title="Evento"></div></td>
-									        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
-									        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
-			      						</tr>
-			      						<tr>
-			        						<td>UNESCO E CISAC LANÇAM MAPEAMENTO GLOBAL DAS INDÚSTRIAS CULTURAIS E CRIATIVAS</td>
-			        						<td class="hidden-xs">04/10/2016</td>
-									        <td class="hidden-xs"><div class="tableAdm-icon news" title="Notícia"></div></td>
-									        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
-									        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
-			      						</tr>
-			      						<tr>
-									        <td>EMPREENDEDORES BRASILEIROS PARTICIPAM DO MICSUL 2016</td>
-									        <td class="hidden-xs">03/10/2016</td>
-									        <td class="hidden-xs"><div class="tableAdm-icon event" title="Evento"></div></td>
-									        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
-									        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
-			      						</tr>
+								<div class="contentAdm-subSection">
 
-			    					</tbody>
+									<div class="subSection-title">
+										<div class="subSectionRight-arrow"></div>
+										<div class="text-title">NOTÍCIAS</div>
+									</div>
 
-			  					</table>
+									<div class="subSection-content">
 
-			  					<!-- paginação! -->
-			  					<ul class="pagination dark">
-									<li class="active"><a href="#">1</a></li>
-									<li><a href="#">2</a></li>
-									<li><a href="#">3</a></li>
-									<li><a href="#">4</a></li>
-									<li><a href="#">5</a></li>
-								</ul>
+										<table class="table table-hover admin-table">
+					    					
+					    					<thead>
+					      						<tr>
+					        						<th>Título</th>
+					        						<th class="hidden-xs">Data</th>
+					        						<th class="hidden-xs">Tipo</th>
+					        						<th>Editar</th>
+					        						<th>Excluir</th>
+					      						</tr>
+					    					</thead>
+					    					
+					    					<tbody>
+					      						<tr>
+					        						<td>UNESCO E CISAC LANÇAM MAPEAMENTO GLOBAL DAS INDÚSTRIAS CULTURAIS E CRIATIVAS</td>
+					        						<td class="hidden-xs">04/10/2016</td>
+											        <td class="hidden-xs"><div class="tableAdm-icon news" title="Notícia"></div></td>
+											        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
+											        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
+					      						</tr>
+					      						<tr>
+											        <td>EMPREENDEDORES BRASILEIROS PARTICIPAM DO MICSUL 2016</td>
+											        <td class="hidden-xs">03/10/2016</td>
+											        <td class="hidden-xs"><div class="tableAdm-icon event" title="Evento"></div></td>
+											        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
+											        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
+					      						</tr>
+					      						<tr>
+					        						<td>UNESCO E CISAC LANÇAM MAPEAMENTO GLOBAL DAS INDÚSTRIAS CULTURAIS E CRIATIVAS</td>
+					        						<td class="hidden-xs">04/10/2016</td>
+											        <td class="hidden-xs"><div class="tableAdm-icon news" title="Notícia"></div></td>
+											        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
+											        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
+					      						</tr>
+					      						<tr>
+											        <td>EMPREENDEDORES BRASILEIROS PARTICIPAM DO MICSUL 2016</td>
+											        <td class="hidden-xs">03/10/2016</td>
+											        <td class="hidden-xs"><div class="tableAdm-icon event" title="Evento"></div></td>
+											        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
+											        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
+					      						</tr>
+					      						<tr>
+					        						<td>UNESCO E CISAC LANÇAM MAPEAMENTO GLOBAL DAS INDÚSTRIAS CULTURAIS E CRIATIVAS</td>
+					        						<td class="hidden-xs">04/10/2016</td>
+											        <td class="hidden-xs"><div class="tableAdm-icon news" title="Notícia"></div></td>
+											        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
+											        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
+					      						</tr>
+					      						<tr>
+											        <td>EMPREENDEDORES BRASILEIROS PARTICIPAM DO MICSUL 2016</td>
+											        <td class="hidden-xs">03/10/2016</td>
+											        <td class="hidden-xs"><div class="tableAdm-icon event" title="Evento"></div></td>
+											        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
+											        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
+					      						</tr>
+					      						<tr>
+					        						<td>UNESCO E CISAC LANÇAM MAPEAMENTO GLOBAL DAS INDÚSTRIAS CULTURAIS E CRIATIVAS</td>
+					        						<td class="hidden-xs">04/10/2016</td>
+											        <td class="hidden-xs"><div class="tableAdm-icon news" title="Notícia"></div></td>
+											        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
+											        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
+					      						</tr>
+					      						<tr>
+											        <td>EMPREENDEDORES BRASILEIROS PARTICIPAM DO MICSUL 2016</td>
+											        <td class="hidden-xs">03/10/2016</td>
+											        <td class="hidden-xs"><div class="tableAdm-icon event" title="Evento"></div></td>
+											        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
+											        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
+					      						</tr>
+					      						<tr>
+					        						<td>UNESCO E CISAC LANÇAM MAPEAMENTO GLOBAL DAS INDÚSTRIAS CULTURAIS E CRIATIVAS</td>
+					        						<td class="hidden-xs">04/10/2016</td>
+											        <td class="hidden-xs"><div class="tableAdm-icon news" title="Notícia"></div></td>
+											        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
+											        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
+					      						</tr>
+					      						<tr>
+											        <td>EMPREENDEDORES BRASILEIROS PARTICIPAM DO MICSUL 2016</td>
+											        <td class="hidden-xs">03/10/2016</td>
+											        <td class="hidden-xs"><div class="tableAdm-icon event" title="Evento"></div></td>
+											        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
+											        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
+					      						</tr>
+
+					    					</tbody>
+
+					  					</table>
+
+					  					<!-- paginação! -->
+					  					<ul class="pagination dark">
+											<li class="active"><a href="#">1</a></li>
+											<li><a href="#">2</a></li>
+											<li><a href="#">3</a></li>
+											<li><a href="#">4</a></li>
+											<li><a href="#">5</a></li>
+										</ul>
+
+									</div>
+								</div>
+								<div class="contentAdm-subSection">
+
+									<div class="subSection-title">
+										<div class="subSectionRight-arrow"></div>
+										<div class="text-title">EVENTOS</div>
+									</div>
+
+									<div class="subSection-content">
+
+										<table class="table table-hover admin-table">
+					    					
+					    					<thead>
+					      						<tr>
+					        						<th>Título</th>
+					        						<th>Local</th>
+					        						<th class="hidden-xs">Data</th>
+					        						<th>Editar</th>
+					        						<th>Excluir</th>
+					      						</tr>
+					    					</thead>
+					    					
+					    					<tbody>
+					      						<tr>
+					        						<td>Mercado de Indústrias Culturais do Sul (MICSUL 2016)</td>
+					        						<td>Bogotá, Colômbia</td>
+					        						<td class="hidden-xs">17/10/2016</td>
+											        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
+											        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
+					      						</tr>
+					      						<tr>
+					        						<td>Mercado de Indústrias Culturais do Sul (MICSUL 2016)</td>
+					        						<td>Bogotá, Colômbia</td>
+					        						<td class="hidden-xs">17/10/2016</td>
+											        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
+											        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
+					      						</tr>
+					      						<tr>
+					        						<td>Mercado de Indústrias Culturais do Sul (MICSUL 2016)</td>
+					        						<td>Bogotá, Colômbia</td>
+					        						<td class="hidden-xs">17/10/2016</td>
+											        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
+											        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
+					      						</tr>
+					      						<tr>
+					        						<td>Mercado de Indústrias Culturais do Sul (MICSUL 2016)</td>
+					        						<td>Bogotá, Colômbia</td>
+					        						<td class="hidden-xs">17/10/2016</td>
+											        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
+											        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
+					      						</tr>
+					      						<tr>
+					        						<td>Mercado de Indústrias Culturais do Sul (MICSUL 2016)</td>
+					        						<td>Bogotá, Colômbia</td>
+					        						<td class="hidden-xs">17/10/2016</td>
+											        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
+											        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
+					      						</tr>
+					      						<tr>
+					        						<td>Mercado de Indústrias Culturais do Sul (MICSUL 2016)</td>
+					        						<td>Bogotá, Colômbia</td>
+					        						<td class="hidden-xs">17/10/2016</td>
+											        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
+											        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
+					      						</tr>
+					      						<tr>
+					        						<td>Mercado de Indústrias Culturais do Sul (MICSUL 2016)</td>
+					        						<td>Bogotá, Colômbia</td>
+					        						<td class="hidden-xs">17/10/2016</td>
+											        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
+											        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
+					      						</tr>
+					      						<tr>
+					        						<td>Mercado de Indústrias Culturais do Sul (MICSUL 2016)</td>
+					        						<td>Bogotá, Colômbia</td>
+					        						<td class="hidden-xs">17/10/2016</td>
+											        <td><div class="tableAdm-icon edit" title="Editar"></div></td>
+											        <td><div class="tableAdm-icon bin" title="Excluir"></div></td>
+					      						</tr>
+					    					</tbody>
+
+					  					</table>
+
+					  					<!-- paginação! -->
+					  					<ul class="pagination dark">
+											<li class="active"><a href="#">1</a></li>
+											<li><a href="#">2</a></li>
+											<li><a href="#">3</a></li>
+											<li><a href="#">4</a></li>
+											<li><a href="#">5</a></li>
+										</ul>
+
+									</div>
+								</div>
 
 							</div>
 						</div>
